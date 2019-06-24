5d43cf6b5aaaa44ce44aa024a531d3827643c1a8
Thiago Tourinho <thiago.tourinho@outlook.com>
2019-06-24 15:13:04 -0300

    atualizações

--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -18,7 +18,7 @@ namespace QuickBuy.Dominio.Entidades
         /// <summary>
         /// Um Usuário pode ter nenhum ou muitos pedidos
         /// </summary>
-        public ICollection<Pedido> Pedidos { get; set; }
+        public virtual ICollection<Pedido> Pedidos { get; set; }
 
         public override void Validate()
         {
